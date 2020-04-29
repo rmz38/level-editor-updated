de83964e666757fc14e8b46a8b12721d1eebd200
Rose Zhou <rmz38@cornell.edu>
2020-04-29 00:12:54 -0400

    Updates

---
+++ precache-manifest.09ff5295112150bb622662a02c7191ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039aaf7714dad3f9c942392014ef9d3e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f4e39993b44e8b9146d0",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6ae3cf48509d17035dda",
+    "url": "./static/css/main.3074bda4.chunk.css"
+  },
+  {
+    "revision": "f4e39993b44e8b9146d0",
+    "url": "./static/js/2.f0d7d32d.chunk.js"
+  },
+  {
+    "revision": "56e689dbd9bce3e7361a42ca28de67b9",
+    "url": "./static/js/2.f0d7d32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae3cf48509d17035dda",
+    "url": "./static/js/main.29dd1169.chunk.js"
+  },
+  {
+    "revision": "f83277a787de122063fb",
+    "url": "./static/js/runtime-main.96555c95.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/level-editor-updated/precache-manifest.70792a49ef5949cb23b8dda1c000eda3.js"
+  "./precache-manifest.09ff5295112150bb622662a02c7191ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/level-editor-updated/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
